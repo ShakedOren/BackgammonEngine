4984a8a2c6f4a0c4edea97be3c4714312e97041d
Shaked Oren <shakedoren957@gmail.com>
2020-10-10 23:09:03 +0300

    move methods to templates

--- BackgammonEngine/BackgammonEngine/Board.cpp
+++ BackgammonEngine/BackgammonEngine/Board.cpp
@@ -6,8 +6,8 @@ namespace Backgammon
 Board::Board()
 	: board_(NUM_OF_POINTS_ON_BOARD)
 {
-	init_white_points();
-	init_black_points();
+	init_board<>(board_.begin(), Chip::Color::WHITE);
+	init_board<>(board_.rbegin(), Chip::Color::BLACK);
 }
 
 void Board::print_board()
@@ -21,43 +21,14 @@ void Board::print_board()
 	std::cout << std::endl;
 }
 
-void Board::init_white_points()
-{
-	auto chip_color = Chip::Color::WHITE;
-	auto board_start = board_.begin();
-
-	board_start->set_num_of_chips_on_point(2, chip_color);
-	(board_start + 11)->set_num_of_chips_on_point(5, chip_color);
-	(board_start + 16)->set_num_of_chips_on_point(3, chip_color);
-	(board_start + 18)->set_num_of_chips_on_point(5, chip_color);
-}
-
-void Board::init_black_points()
-{
-	auto chip_color = Chip::Color::BLACK;
-	auto board_start = board_.rbegin();
-
-	board_start->set_num_of_chips_on_point(2, chip_color);
-	(board_start + 11)->set_num_of_chips_on_point(5, chip_color);
-	(board_start + 16)->set_num_of_chips_on_point(3, chip_color);
-	(board_start + 18)->set_num_of_chips_on_point(5, chip_color);
-}
 void Board::move_white_chip(int start_pos, int end_pos)
 {
-	auto chip_color = Chip::Color::WHITE;
-	auto board_start = board_.begin();
-
-	(board_start + start_pos)->remove_last_chip();
-	(board_start + end_pos)->add_chip(chip_color);
+	move_chip(board_.begin(), start_pos, end_pos, Chip::Color::WHITE);
 }
 
 void Board::move_black_chip(int start_pos, int end_pos)
 {
-	auto chip_color = Chip::Color::BLACK;
-	auto board_start = board_.rbegin();
-
-	(board_start + start_pos)->remove_last_chip();
-	(board_start + end_pos)->add_chip(chip_color);
+	move_chip(board_.rbegin(), start_pos, end_pos, Chip::Color::BLACK);
 }
 
 } // namespace Backgammon
--- BackgammonEngine/BackgammonEngine/Board.h
+++ BackgammonEngine/BackgammonEngine/Board.h
@@ -22,10 +22,21 @@ public:
 private:
 	std::vector<Point> board_;
 
-private:
-	void init_white_points();
-	void init_black_points();
 };	
+
+template <class T>
+void init_board(T board_start, Chip::Color chip_color) {
+	board_start->set_num_of_chips_on_point(2, chip_color);
+	(board_start + 11)->set_num_of_chips_on_point(5, chip_color);
+	(board_start + 16)->set_num_of_chips_on_point(3, chip_color);
+	(board_start + 18)->set_num_of_chips_on_point(5, chip_color);
+}
+
+template <class T>
+void move_chip(T board_start, int start_pos, int end_pos, Chip::Color chip_color) {
+	(board_start + start_pos)->remove_last_chip();
+	(board_start + end_pos)->add_chip(chip_color);
+}
 
 } // namespace Backgammon
 
